94a0fc3a3d38de83f04b05ec2f830cdfd1ba391b
maxsmg <maxsmg@mail.ru>
2026-06-26 19:05:41 +0700

    Add projects "ApiGateway" and "Transactions"

--- PersonalFinanceTracker.AppHost/AppHost.cs
+++ PersonalFinanceTracker.AppHost/AppHost.cs
@@ -1,3 +1,7 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.ApiGateway>("apigateway");
+
+builder.AddProject<Projects.Transactions>("transactions");
+
 builder.Build().Run();
