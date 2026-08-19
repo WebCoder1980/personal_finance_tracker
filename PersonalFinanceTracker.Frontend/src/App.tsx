210a77cee9577813a712f9efa40e1cb3ecada9ec
maxsmg <maxprogger@mail.ru>
2026-08-19 16:14:22 +0700

    Add shadcn support to frontend.

--- PersonalFinanceTracker.Frontend/src/App.tsx
+++ PersonalFinanceTracker.Frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
+import {Button} from "@/components/ui/button.tsx";
 
 function App() {
   return (
     <>
-      <div className="flex h-25 justify-center items-center text-3xl my-3 mx-[40%] bg-yellow-300"><div>Привет, мир</div></div>
+      <div className="flex flex-col h-25 justify-center items-center text-3xl my-3 mx-[40%] bg-yellow-300">
+          <div>Привет, мир</div>
+          <Button onClick={() => alert("Клик...")}>Клик</Button>
+      </div>
+
     </>
   )
 }
