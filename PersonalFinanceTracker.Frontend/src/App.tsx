d9061eb7910fe9ae5c284fe4db09364d07ce3313
maxsmg <maxprogger@mail.ru>
2026-08-19 15:55:17 +0700

    Add tailwind to frontend.

--- PersonalFinanceTracker.Frontend/src/App.tsx
+++ PersonalFinanceTracker.Frontend/src/App.tsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <>
-
+      <div className="flex h-25 justify-center items-center text-3xl my-3 mx-[40%] bg-yellow-300"><div>Привет, мир</div></div>
     </>
   )
 }
